c12abbcc570354d95dd9418c9adb94685876e7c3
sravshanbek <sravshanbek@it.gov.kg>
2021-07-22 20:40:26 +0600

    basics configs

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'require-jsdoc': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'semi': 'off',
+  },
+};
